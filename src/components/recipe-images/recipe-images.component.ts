bcc1b8814f1b507892632f09c070f75545c4dd5d
Emil Berbakov <emberbakov@gmail.com>
2023-05-08 20:45:02 -0500

    recipe card beginnings

--- src/components/recipe-images/recipe-images.component.ts
+++ src/components/recipe-images/recipe-images.component.ts
@@ -1,13 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {MatCardModule} from '@angular/material/card';
+import { RecipePreview } from '../recipe/recipe.model';
+import { RecipeService } from 'src/components/recipe/recipe.service';
 
 @Component({
   selector: 'app-recipe-images',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, MatCardModule],
   templateUrl: './recipe-images.component.html',
   styleUrls: ['./recipe-images.component.scss']
 })
-export class RecipeImagesComponent {
 
+export class RecipeImagesComponent implements OnInit {
+  @Input({ required: true }) recipeID!: number;
+  recipePreview$ = this.recipeService.recipeCardInfo$;
+  constructor(
+    private recipeService: RecipeService
+  ) {}
+
+  ngOnInit(): void {
+    this.recipeService.getRecipeCardInfo(this.recipeID);
+  }
 }
---
+++ src/components/recipe/recipe.model.ts
@@ -0,0 +1,5 @@
+export interface RecipePreview  {
+  cardImage: string,
+  altText: string,
+  cardContent: string
+}
